8e67f54e3799793cbdded133e0db0b23bfb6b29f
jienfak <jienfak@protonmail.com>
2020-01-17 01:27:32 +0500

    Yet another simplification about environment setting

--- dot/setenv.sh
+++ dot/setenv.sh
@@ -11,31 +11,28 @@ noext(){
 }
 
 setenv(){
-	if [ -d "$ENVDIR" ] ; then
-		# The way to make system more flexible.
-		# 	Actually I'm really tired of editing one big file.
-		# 	It is really easier to change specific one with variables
-		# 	or modules.
+if [ -d "$ENVDIR" ] ; then
+	# The way to make system more flexible.
+	# 	Actually I'm really tired of editing one big file.
+	# 	It is really easier to change specific one with variables
+	# 	or modules.
 
-		# Set variables from files.
-		sh="$1"
-		
-		for i in `echo $ENVDIR/*."$sh".var` ; do
-			noext1="`noext $i`"
-			noext2="`noext $noext1`"
-			if test -r "$i"  ; then
-				cat="` cat \"$i\" `"
-				export "$noext2"="$cat"
-			fi
-		done
+	# Set variables from files.
+	sh="$1"
+	
+	for i in  "$ENVDIR/$sh"/*.var ; do
+		noext="`noext $i`"
+		if test -r "$i"  ; then
+			cat="`cat \"$i\" `"
+			export "$noext"="$cat"
+		fi
+	done
 
-		# Modules.
-		for i in $ENVDIR/*."$sh".sh ; do
-			if [ -r "$i" ] ; then
-
-				. "$i"
-			fi
-		done
-	fi
+	# Modules.
+	for i in "$ENVDIR/$sh"/*.sh ; do
+		if test -r "$i"  ; then
+			. "$i"
+		fi
+	done
+fi
 }
-
