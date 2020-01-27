344837e47e91c16fcf12fc4547c9e2470313235e
jienfak <jienfak@protonmail.com>
2020-01-27 20:52:03 +0500

    Fixed color setting in virtual console of Linux with POSIX shells

--- dot/login.sh
+++ dot/login.sh
@@ -4,21 +4,21 @@ if [ "$TERM" = linux ] ; then
 	# Running from Linux virtual console.
 	
 	# Virtual console colors.
-	echo -en "\e]P0000000" # Black.
-	echo -en "\e]P8666666" # Darkgrey.
-	echo -en "\e]P1DD2222" # Darkred.
-	echo -en "\e]P9FF0000" # Red.
-	echo -en "\e]P267E300" # Darkgreen.
-	echo -en "\e]PAA9F16C" # Green.
-	echo -en "\e]P3F7FF00" # Brown.
-	echo -en "\e]PBEBF22C" # Yellow..
-	echo -en "\e]P45190D0" # Darkblue.
-	echo -en "\e]PC346AA1" # Blue.
-	echo -en "\e]P5FF6E00" # Darkmagenta.
-	echo -en "\e]PDDE6000" # Magenta.
-	echo -en "\e]P600CFCF" # Darkcyan.
-	echo -en "\e]PE00FFFF" # Cyan.
-	echo -en "\e]P7DDDDDD" # Lightgrey.
-	echo -en "\e]PFFFFFFF" # White.
+	printf "\033]P0000000" # Black.
+	printf "\033]P8666666" # Darkgrey.
+	printf "\033]P1DD2222" # Darkred.
+	printf "\033]P9FF0000" # Red.
+	printf "\033]P267E300" # Darkgreen.
+	printf "\033]PAA9F16C" # Green.
+	printf "\033]P3F7FF00" # Brown.
+	printf "\033]PBEBF22C" # Yellow..
+	printf "\033]P45190D0" # Darkblue.
+	printf "\033]PC346AA1" # Blue.
+	printf "\033]P5FF6E00" # Darkmagenta.
+	printf "\033]PDDE6000" # Magenta.
+	printf "\033]P600CFCF" # Darkcyan.
+	printf "\033]PE00FFFF" # Cyan.
+	printf "\033]P7DDDDDD" # Lightgrey.
+	printf "\033]PFFFFFFF" # White.
 	#clear # For background artifacting.
 fi
